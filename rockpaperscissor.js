704ca65e41ca3b9f7668f25e9684dff3c1a3e56c
Friedrich <friedrich.ten.hagen@googlemail.com>
2022-08-06 21:37:47 +0200

    Create external js file, start rough outline via pseudo code

---
+++ rockpaperscissor.js
@@ -0,0 +1,14 @@
+console.log("Lets play rock paper scissor!");
+
+function consoleTest() {
+    console.log("is this working?");
+}
+
+
+// create function that generates random choice for computer
+// rock, paper, scissor
+
+//function that take input from human (toLowerCase to ensure comparablitiy with CompChoice)
+
+//function that compares CompChoice and HumanChoice
+//and outputs result
