ae2b9ee165618960b43a3b07dfccc03c4acb5922
Friedrich <friedrich.ten.hagen@googlemail.com>
2022-08-07 12:19:52 +0200

    Fix syntax errors (missing parentheses after functions) to get game logic working!

--- rockpaperscissor.js
+++ rockpaperscissor.js
@@ -5,7 +5,16 @@ console.log("Lets play rock paper scissor!");
 // create function that generates random choice for computer
 // three options: rock, paper, scissor
 function compChoice(){
-    return Math.floor(Math.random()*3);
+    let hand = Math.floor(Math.random()*3);
+    if(hand == 0){
+        return "rock";
+    }
+    else if(hand == 1){
+        return "paper";
+    }
+    else{
+        return "scissor";
+    }
 }
 
 
@@ -13,19 +22,43 @@ function compChoice(){
 function humanChoice(){
     const val = document.querySelector('input').value;
     const lowerCaseVal = val.toLowerCase();
-    console.log(lowerCaseVal);
+    if(lowerCaseVal === "rock" || lowerCaseVal === "paper" || lowerCaseVal === "scissor"){
+        return lowerCaseVal;
+    }
+    else{
+        console.log("Come back when you understand the game, kid!");
+        return "Error";
+    }
 }
-
-
 
 
 //function that compares compChoice and humanChoice
 //and outputs result
-/*
-if(chompChoice === humanChoice){
-    console.log("It's a draw!");
+
+function evalGame(){
+    if(compChoice() === humanChoice()){
+        console.log("It's a draw!");
+    }
+    else if(compChoice() === "rock" && humanChoice() === "paper"){
+        console.log("Human wins! Paper beats Rock!");
+    }
+    else if(compChoice() === "rock" && humanChoice() === "scissor"){
+        console.log("Computer wins! Rock beats Scissor");
+    }
+    else if(compChoice() === "paper" && humanChoice() === "rock"){
+        console.log("Comp wins! paper beats rock!");
+    }
+    else if(compChoice() === "paper" && humanChoice() === "scissor"){
+        console.log("Human wins! scissor beats paper!");
+    }
+    else if(compChoice() === "scissor" && humanChoice() === "paper"){
+        console.log("comp wins! scissor beats paper!");
+    }
+    else if(compChoice() === "scissor" && humanChoice() === "rock"){
+        console.log("Human wins! rock beats scissor!");
+    }
+    else{
+        console.log("something went horribly wrong...");
+    }
 }
-else if(compChoice === rock && humanChoice === paper){
-    console.log("Human wins!");
-}
-*/
+
