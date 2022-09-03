0643889bba1ce4041a9a321ef0cfb746b23361d4
Friedrich <friedrich.ten.hagen@googlemail.com>
2022-09-03 15:50:54 +0200

    Create game logic on button click. Style buttons.

--- rockpaperscissor.js
+++ rockpaperscissor.js
@@ -10,88 +10,44 @@ function compChoice(){
         return "paper";
     }
     else{
-        return "scissor";
+        return "scissors";
     }
 }
 
-
-//function that takes input from human (toLowerCase to ensure comparablitiy with CompChoice)
-function humanChoice(){
-    const val = document.querySelector('input').value;
-    const lowerCaseVal = val.toLowerCase();
-    if(lowerCaseVal === "rock" || lowerCaseVal === "paper" || lowerCaseVal === "scissor"){
-        return lowerCaseVal;
-    }
-    else{
-        console.log("Come back when you understand the game, kid!");
-        return "Error";
-    }
-}
+const buttons = document.querySelectorAll("button")
+buttons.forEach(button => {
+    button.addEventListener("click", e => {
+        console.log(evalGame(e))
+    })
+})
 
 
-
-// repeat game 5 times 
-
-function evalGame(){
-    let alertChoice = prompt("Enter your choice");
-    const lowerChoice = alertChoice.toLowerCase();
+function evalGame(e){
+    const lowerChoice = e.target.className.toLowerCase();
     let compChoiceTemp = compChoice();
 
-            if(lowerChoice === compChoiceTemp){
-                return "It's a draw!";
-            }
-            else if(compChoiceTemp === "rock" && lowerChoice === "paper"){
-                return "human wins!" // Paper beats Rock!";
-            }
-            else if(compChoiceTemp === "rock" && lowerChoice === "scissor"){
-                return "comp wins!" //Rock beats Scissor";
-            }
-            else if(compChoiceTemp === "paper" && lowerChoice === "rock"){
-                return "comp wins!" // paper beats rock!";
-            }
-            else if(compChoiceTemp === "paper" && lowerChoice === "scissor"){
-                return "human wins!" // scissor beats paper!";
-            }
-            else if(compChoiceTemp === "scissor" && lowerChoice === "paper"){
-                return "comp wins!" // scissor beats paper!";
-            }
-            else if(compChoiceTemp === "scissor" && lowerChoice === "rock"){
-                return "human wins!" // rock beats scissor!
-            }
-            else{
-                return "something went horribly wrong...";
-            }
-}   
-
-
-
-function fiveGames(){
-    let compScore = 0;
-    let humanScore = 0;
-
-    for(let i=0; i<5; i++){
-        let resultGame = evalGame();
-        console.log(resultGame);
-
-        if(resultGame === "human wins!"){
-            humanScore++;
+        if(lowerChoice === compChoiceTemp){
+            return "It's a draw!";
         }
-        else if(resultGame === "comp wins!"){
-            compScore++;
+        else if(compChoiceTemp === "rock" && lowerChoice === "paper"){
+            return "human wins!" // Paper beats Rock!";
+        }
+        else if(compChoiceTemp === "rock" && lowerChoice === "scissors"){
+            return "comp wins!" //Rock beats Scissor";
+        }
+        else if(compChoiceTemp === "paper" && lowerChoice === "rock"){
+            return "comp wins!" // paper beats rock!";
+        }
+        else if(compChoiceTemp === "paper" && lowerChoice === "scissors"){
+            return "human wins!" // scissor beats paper!";
+        }
+        else if(compChoiceTemp === "scissors" && lowerChoice === "paper"){
+            return "comp wins!" // scissor beats paper!";
+        }
+        else if(compChoiceTemp === "scissors" && lowerChoice === "rock"){
+            return "human wins!" // rock beats scissor!
         }
         else{
-            
+            return "something went horribly wrong...";
         }
-        console.log("human score: " + humanScore +" computer score: "+ compScore);
-       
-    }
-    if(humanScore < compScore){
-        console.log("Computer wins!!!!!!!!!!!!!!!!!!")
-    }
-    else if(compScore < humanScore){
-        console.log("human wins!!!!!!!!!!!!!!!!!!!!!!!1")
-    }
-    else{
-        console.log("It is a draw!!!!!!!!!!!!!!!!!!!!!!!")
-    }
-}
+}   
